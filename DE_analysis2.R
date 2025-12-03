507f1a2d6da27377ffdd8afbdef47b411c8142ce
Şehriban Büyükkılıç <115555604+codewithsehriban@users.noreply.github.com>
2025-12-03 03:51:35 -0800

    Add files via upload
    Article_ML_model.ipynb all ML codes which are used during our work 
    data_cleaning_for_ML.ipynb how we cleaned our data for this work 
    DE_analysis2.R for DE analysis

---
+++ DE_analysis2.R
@@ -0,0 +1,63 @@
+library(Rsubread)
+library(BiocParallel)
+library(limma)
+library(edgeR)
+library(GenomicRanges)
+library(rtracklayer)
+library(stringr)
+library(dplyr)
+library(data.table)
+
+# Set working directory
+setwd("/archive/alotaibih/sehribanb/new_mouse/d102/input/")
+
+# Get file paths for BAM files in directories
+bams_S <- list.files(path = ".", pattern = ".bam", full.names = TRUE)
+
+# Run featureCounts for single-end data
+counts_S <- featureCounts(files = bams_S,
+                          annot.ext = "/archive/alotaibih/sehribanb/new_mouse/tgf_beta/CustomizedGTF_NT_gencode.vM23.annotation.gtf", 
+                          isGTFAnnotationFile = TRUE,
+                          GTF.featureType = "exon",
+                          GTF.attrType = "gene_id",
+                          useMetaFeatures = TRUE,
+                          allowMultiOverlap = FALSE,
+                          largestOverlap = FALSE,
+                          countMultiMappingReads = TRUE,
+                          isPairedEnd = TRUE,
+                          nthreads = 10)
+
+# Extract counts and annotations for single-end data
+counts_S_df_C <- as.data.frame(counts_S$counts)
+counts_S_df_A <- as.data.frame(counts_S$annotation)
+counts_S_df_C$GeneID <- row.names(counts_S_df_C)
+counts_S_df_C_A <- merge(counts_S_df_A, counts_S_df_C, by = "GeneID")
+
+# Import annotation file
+gtf <- rtracklayer::import("/archive/alotaibih/sehribanb/new_mouse/tgf_beta/CustomizedGTF_NT_gencode.vM23.annotation.gtf")
+gtf_data_frame <- as.data.frame(gtf)
+
+# Filter annotation data
+gtf_filtered <- select(gtf_data_frame, c(10, 12, 17))
+gtf_filtered_uniq <- gtf_filtered %>% distinct(gene_id, .keep_all = TRUE)
+
+# Add gene IDs as a column
+counts_S_df_C_A$gene_id <- row.names(counts_S_df_C_A)
+
+# Merge counts and annotations
+counts_df_annotated <- merge(gtf_filtered_uniq, counts_S_df_C_A, by = "gene_id")
+
+# Write annotated counts to file
+write.table(counts_df_annotated, "/archive/alotaibih/sehribanb/new_mouse/d102/counts_df_annotated.tsv", quote = FALSE, col.names = TRUE, sep = "\t")
+
+# Calculate FPKM values
+z <- DGEList(counts = counts_S$counts, genes = counts_S_df_C_A[, c("GeneID", "Length")])
+n <- calcNormFactors(z)
+RPKM <- rpkm(n)
+fp <- as.data.frame(RPKM)
+fp$gene_id <- rownames(fp)
+FPKM <- fp %>% select(gene_id, everything())
+FPKM_annotated <- merge(FPKM, gtf_filtered_uniq, by = "gene_id")
+
+# Write FPKM values to file
+write.table(FPKM_annotated, "/archive/alotaibih/sehribanb/new_mouse/d102/fpkm_annotated.tsv", quote = FALSE, col.names = TRUE, sep = "\t")
