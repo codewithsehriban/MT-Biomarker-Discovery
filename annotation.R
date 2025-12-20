190941583e1a93ef5e828568ba71a899113ce5d0
Şehriban Büyükkılıç <115555604+codewithsehriban@users.noreply.github.com>
2025-12-20 01:39:41 -0800

    Update file paths in annotation.R

--- annotation.R
+++ annotation.R
@@ -9,14 +9,14 @@ library(stringr)
 library(dplyr)
 library(data.table)
 
-#feature_counts####
+#featurecounts
 # Set working directory to where your BAM files are located
-setwd("/archive/alotaibih/sehribanb/mouse_normal/d1/input/")
+setwd("/archive/d1/input/")
 # Obtain BAM file path
 bams <- list.files(pattern = ".bam", full.names = TRUE)
 
 counts <- featureCounts(files = bams,
-                          annot.ext = "/archive/alotaibih/sehribanb/new_mouse/tgf_beta/gtf", 
+                          annot.ext = "/archive/mouse_gtf", 
                           isGTFAnnotationFile=TRUE,
                           GTF.featureType= c("exon"),
                           GTF.attrType="gene_id",
@@ -29,11 +29,11 @@ counts <- featureCounts(files = bams,
 )
 
 counts_df <- as.data.frame(counts$counts)
-write.table(counts$counts, "/archive/alotaibih/sehribanb/mouse_normal/d1/counts.tsv", quote = F, col.names = T, sep = "\t")
+write.table(counts$counts, "/archive/d1/counts.tsv", quote = F, col.names = T, sep = "\t")
 
 #calculate fpkm values
 z <- DGEList(counts=counts$counts, genes=counts$annotation[,c("GeneID","Length")])
 z<- calcNormFactors(z)
 RPKM <-rpkm(z)
-write.table(RPKM, "/archive/alotaibih/sehribanb/mouse_normal/d1/FPKM.tsv", quote = F, col.names = T, sep = "\t")
+write.table(RPKM, "/archive/d1/FPKM.tsv", quote = F, col.names = T, sep = "\t")
 done
